30968d50f38e9d95105b566d7ab9ae5a759ef54b
Andre Posmitny <40007650+AndrePosm@users.noreply.github.com>
2021-02-23 22:03:57 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+// 1. Создать функцию, которая возвращает true/false, на вход принимает обьект,
+//в котором есть поле age, и если age > 18 ? true : false
 
-zdfsdf
+//const user = { age: 21 }
+
+
+const user = {
+   age: 21,
+};
+
+tiketToTheClub(age) {
+  if (age >18){
+   return true;
+  } else {
+   return false;
+  }
+}
+
+
